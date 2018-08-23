60a1615c7368e588a0a46e4a78ebe4644ea3fe80
king-of-spades <kegr@microsoft.com>
2018-08-23 14:37:29 -0700

    initial commit of working project

---
+++ Xamarin.UITest/PermissionPopups/SpeakToMe/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using UIKit;
+
+namespace SpeakToMe
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public override UIWindow Window { get; set; }
+
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+
+#if ENABLE_TEST_CLOUD
+            Xamarin.Calabash.Start();
+#endif
+
+            return true;
+
+        }
+    }
+}
